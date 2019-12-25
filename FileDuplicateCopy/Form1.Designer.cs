9e6b9cb60eae64caa4eb2c7d943dbc23e2fcb985
DamijanD <damijan.dolenec@gmail.com>
2019-12-25 18:57:52 +0100

    Initial commit

---
+++ FileDuplicateCopy/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace FileDuplicateCopy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbOriginalLocation = new System.Windows.Forms.TextBox();
+            this.tbDestination = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.tbExt = new System.Windows.Forms.TextBox();
+            this.chbDelete = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Original location";
+            // 
+            // tbOriginalLocation
+            // 
+            this.tbOriginalLocation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbOriginalLocation.Location = new System.Drawing.Point(12, 25);
+            this.tbOriginalLocation.Name = "tbOriginalLocation";
+            this.tbOriginalLocation.Size = new System.Drawing.Size(684, 20);
+            this.tbOriginalLocation.TabIndex = 1;
+            // 
+            // tbDestination
+            // 
+            this.tbDestination.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbDestination.Location = new System.Drawing.Point(12, 70);
+            this.tbDestination.Name = "tbDestination";
+            this.tbDestination.Size = new System.Drawing.Size(684, 20);
+            this.tbDestination.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Destination";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 106);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(776, 303);
+            this.textBox1.TabIndex = 4;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(713, 415);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 5;
+            this.btnGo.Text = "GO";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 415);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(695, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // tbExt
+            // 
+            this.tbExt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbExt.Location = new System.Drawing.Point(702, 25);
+            this.tbExt.Name = "tbExt";
+            this.tbExt.Size = new System.Drawing.Size(86, 20);
+            this.tbExt.TabIndex = 7;
+            this.tbExt.Text = "*.jpg";
+            // 
+            // chbDelete
+            // 
+            this.chbDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chbDelete.AutoSize = true;
+            this.chbDelete.Location = new System.Drawing.Point(725, 72);
+            this.chbDelete.Name = "chbDelete";
+            this.chbDelete.Size = new System.Drawing.Size(57, 17);
+            this.chbDelete.TabIndex = 8;
+            this.chbDelete.Text = "Delete";
+            this.chbDelete.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chbDelete);
+            this.Controls.Add(this.tbExt);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbDestination);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbOriginalLocation);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbOriginalLocation;
+        private System.Windows.Forms.TextBox tbDestination;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox tbExt;
+        private System.Windows.Forms.CheckBox chbDelete;
+    }
+}
+
---
+++ FileDuplicateCopy/Form1.cs
@@ -0,0 +1,91 @@
+﻿using ExifLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileDuplicateCopy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            var files = System.IO.Directory.GetFiles(tbOriginalLocation.Text, tbExt.Text);
+
+            progressBar1.Value = 0;
+            progressBar1.Maximum = files.Length;
+            progressBar1.Step = 1;
+
+            foreach (var file in files)
+            {
+                System.IO.FileInfo fi = new System.IO.FileInfo(file);
+
+                string destinationPath = "";
+                if (fi.LastWriteTime.Year == DateTime.Now.Year)
+                {
+                    destinationPath = System.IO.Path.Combine(tbDestination.Text, fi.LastWriteTime.ToString("yyyy_MM_dd"), fi.Name);
+                }
+                else
+                {
+                    destinationPath = System.IO.Path.Combine(tbDestination.Text, fi.LastWriteTime.ToString("yyyy\\\\yyyy_MM_dd"), fi.Name);
+                }
+
+
+                System.IO.FileInfo fiDest = new System.IO.FileInfo(destinationPath);
+
+                if (fiDest.Exists)
+                {
+                    try
+                    {
+                        bool duplicate = false;
+                        using (var exifDest = new ExifReader(fiDest.FullName))
+                        using (var exifSource = new ExifReader(fi.FullName))
+                        {
+                            exifDest.GetTagValue<DateTime>(ExifTags.DateTimeOriginal, out DateTime destDate);
+                            exifSource.GetTagValue<DateTime>(ExifTags.DateTimeOriginal, out DateTime sourceDate);
+
+                            if (destDate == sourceDate)
+                            {
+                                duplicate = true;
+                            }
+                        }
+
+                        if (duplicate)
+                        {
+                            if (chbDelete.Checked)
+                                System.IO.File.Delete(fi.FullName);
+                            else
+                                textBox1.AppendText("DELETE\t" + fi.FullName + Environment.NewLine);
+                        }
+                        else
+                        {
+                            textBox1.AppendText(fi.FullName + Environment.NewLine);
+                        }
+                    }
+                    catch (Exception exc)
+                    {
+                        textBox1.AppendText(fi.FullName + " exc: " + exc.Message + Environment.NewLine );
+                    }
+
+                }
+                else
+                {
+                    textBox1.AppendText(fi.FullName + Environment.NewLine);
+                }
+
+                progressBar1.PerformStep();
+
+            }
+        }
+    }
+}
